c13d48a72c45fd8de89d30d68f0650a62dafa957
Jesper Dinger <78754860+Gweppi@users.noreply.github.com>
2022-05-03 09:45:27 +0200

    Update Pizza.swift

--- Pizza.swift
+++ Pizza.swift
@@ -1,3 +1,14 @@
+// Your model:
 struct Pizza: ObservableObject, Identifiable {
-
+@Published public var name: String
+@Published public var diameter: String
+@Published public var price: String
+@Published public var pieces: String
+@Published public var no: String
 }
+
+// Your TextFields:
+TextField("Name", text: $pizza.name)
+TextField("Diameter", text: $pizza.diameter)
+TextField("Pieces", text: $pizza.pieces)
+TextField("Price", text: $pizza.price)
