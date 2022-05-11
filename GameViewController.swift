5abbcab6c325afedc66edd41971e3377bfa72e25
Jesper Dinger <78754860+Gweppi@users.noreply.github.com>
2022-05-11 18:03:12 +0200

    Update GameViewController.swift

--- GameViewController.swift
+++ GameViewController.swift
@@ -1,1 +1,10 @@
+    var score: Int?
 
+    init(score: Int) {
+        self.number = number
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
