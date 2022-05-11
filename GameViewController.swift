48d8c1d697bbca838cb725a77ee30d598fcbddd5
Jesper Dinger <78754860+Gweppi@users.noreply.github.com>
2022-05-11 18:04:48 +0200

    Update GameViewController.swift

--- GameViewController.swift
+++ GameViewController.swift
@@ -1,10 +1,23 @@
+class ViewController: UIViewController {
+    
     var score: Int?
 
     init(score: Int) {
-        self.number = number
+        self.score = score
         super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+// Add a score like this:
+ViewController(score: <your score var here>)
