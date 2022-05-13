e4953707e61afa85d5ada767dbfa979c28195a29
Jesper Dinger <78754860+Gweppi@users.noreply.github.com>
2022-05-13 15:42:33 +0200

    Create dictionary.swift

---
+++ dictionary.swift
@@ -0,0 +1,12 @@
+extension Dictionary where Value: Equatable {
+    func key(forValue value: Value) -> Key? {
+        return first { $0.1 == value }?.0
+        
+    }
+}
+
+let dictionary = ["Nepal": "Kathmandu", "Italy": "Rome", "England": "London"]
+
+let key = dictionary.key(forValue: "Rome")
+
+print(key)
