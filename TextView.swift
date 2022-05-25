06fc2a93d7f4824e59be435a7e121c15df3e97d3
Jesper Dinger <78754860+Gweppi@users.noreply.github.com>
2022-05-25 16:19:39 +0200

    Update TextView.swift

--- TextView.swift
+++ TextView.swift
@@ -1,18 +1,17 @@
 import SwiftUI
 
 struct ContentView: View {
-
-@State private var username = ""
-
-var body: some View {
-TextField("Type here", text: $username)
-}
+	@State private var username = ""
+	
+	var body: some View {
+	TextField("Type here", text: $username)
+	}
 }
 
 struct ViewTheText: View {
-@Binding var username: String
+	@Binding var username: String
   
-  var body: some View {
-Text(username")
+	var body: some View {
+		Text(username)
   }
 }
